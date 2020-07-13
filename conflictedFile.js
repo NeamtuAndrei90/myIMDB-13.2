05d0cf00bdcf29351c4af2e105374564d5d70fa8
Neamtu Andrei <neamtuandrei90@gmail.com>
2020-07-13 19:22:03 +0300

    Group Project

---
+++ conflictedFile.js
@@ -0,0 +1,8 @@
+$ git clone git@github.com:crinel/myIMDB-13.2.git
+Cloning into 'myIMDB-13.2'...
+remote: Enumerating objects: 93, done.
+remote: Counting some local change crina has changed this line: 100% (93/93), done.
+remote: Compressing objects: 100% (66/66), done.
+remote: Total 93 (delta 27), reused 68 (delta 17), pack-reused 0
+Receiving objects: 100% (93/93), 28.45 MiB | 10.99 MiB/s, done.
+Resolving deltas: 100% (27/27), done.
